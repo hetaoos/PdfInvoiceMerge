b972ba36056d2a7e4f0d31118852eed6418a659e
Null <hetaoos@gmail.com>
2018-12-26 20:54:35 +0800

    初始代码

---
+++ PdfInvoiceMerge/frmMain.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace PdfInvoiceMerge
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAdd = new System.Windows.Forms.ToolStripButton();
+            this.btnClear = new System.Windows.Forms.ToolStripButton();
+            this.cmbMergeType = new System.Windows.Forms.ToolStripComboBox();
+            this.btnMerge = new System.Windows.Forms.ToolStripButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.labInfo = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listInvoices = new System.Windows.Forms.ListBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAdd,
+            this.btnClear,
+            this.cmbMergeType,
+            this.btnMerge});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 25);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(758, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(76, 22);
+            this.btnAdd.Text = "添加发票(&A)";
+            this.btnAdd.ToolTipText = "添加发票，支持拖拽添加发票文件";
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(52, 22);
+            this.btnClear.Text = "清空(&C)";
+            this.btnClear.ToolTipText = "清空发票列表，也可以选中后按 DELETE 键删除。";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cmbMergeType
+            // 
+            this.cmbMergeType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMergeType.Items.AddRange(new object[] {
+            "每页一张发票",
+            "每页两张发票",
+            "每页四张发票"});
+            this.cmbMergeType.Name = "cmbMergeType";
+            this.cmbMergeType.Size = new System.Drawing.Size(121, 25);
+            this.cmbMergeType.ToolTipText = "输出样式";
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnMerge.Image = ((System.Drawing.Image)(resources.GetObject("btnMerge.Image")));
+            this.btnMerge.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(56, 22);
+            this.btnMerge.Text = "合并(&M)";
+            this.btnMerge.ToolTipText = "将发票合并后输出。";
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(758, 25);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(58, 21);
+            this.fileToolStripMenuItem.Text = "文件(&F)";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "退出(&X)";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sourceToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 21);
+            this.helpToolStripMenuItem.Text = "帮助(&H)";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.labInfo});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 373);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(758, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // labInfo
+            // 
+            this.labInfo.Name = "labInfo";
+            this.labInfo.Size = new System.Drawing.Size(35, 17);
+            this.labInfo.Text = "就绪.";
+            // 
+            // listInvoices
+            // 
+            this.listInvoices.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listInvoices.FormattingEnabled = true;
+            this.listInvoices.ItemHeight = 21;
+            this.listInvoices.Location = new System.Drawing.Point(0, 50);
+            this.listInvoices.Name = "listInvoices";
+            this.listInvoices.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listInvoices.Size = new System.Drawing.Size(758, 323);
+            this.listInvoices.Sorted = true;
+            this.listInvoices.TabIndex = 4;
+            this.listInvoices.DoubleClick += new System.EventHandler(this.listInvoices_DoubleClick);
+            this.listInvoices.KeyUp += new System.Windows.Forms.KeyEventHandler(this.listInvoices_KeyUp);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "PDF 文件|*.pdf";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "PDF 发票文件|*.pdf";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "PDF 发票";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "关于(&A)";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // sourceToolStripMenuItem
+            // 
+            this.sourceToolStripMenuItem.Name = "sourceToolStripMenuItem";
+            this.sourceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sourceToolStripMenuItem.Text = "源码(&S)";
+            this.sourceToolStripMenuItem.Click += new System.EventHandler(this.sourceToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 395);
+            this.Controls.Add(this.listInvoices);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "PDF 发票合并工具";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frmMain_DragEnter);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel labInfo;
+        private System.Windows.Forms.ListBox listInvoices;
+        private System.Windows.Forms.ToolStripButton btnAdd;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripButton btnClear;
+        private System.Windows.Forms.ToolStripButton btnMerge;
+        private System.Windows.Forms.ToolStripComboBox cmbMergeType;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sourceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ PdfInvoiceMerge/frmMain.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTextSharp.awt.geom;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace PdfInvoiceMerge
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            this.Icon = PdfInvoiceMerge.Properties.Resources.Invoice;
+            RefreshState();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var dlg = openFileDialog1.ShowDialog();
+            if (dlg != DialogResult.OK)
+                return;
+            listInvoices.Items.AddRange(openFileDialog1.FileNames);
+            RefreshState();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listInvoices.Items.Clear();
+            RefreshState();
+        }
+
+        private void RefreshState()
+        {
+            var has = listInvoices.Items.Count > 0;
+            labInfo.Text = has ? $"共 { listInvoices.Items.Count} 张发票" : "就绪";
+            btnClear.Enabled = has;
+            btnMerge.Enabled = has;
+            if (has && listInvoices.SelectedIndex < 0)
+                listInvoices.SelectedIndex = 0;
+        }
+
+        private void listInvoices_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData != Keys.Delete)
+                return;
+            var values = listInvoices.SelectedItems.OfType<object>().ToArray();
+            if (values.Length == 0)
+                return;
+            else if (values.Length == 1)
+            {
+                var index = listInvoices.SelectedIndex;
+                listInvoices.Items.RemoveAt(index);
+                if (index < listInvoices.Items.Count)
+                    listInvoices.SelectedIndex = index;
+                else if (listInvoices.Items.Count > 0)
+                    listInvoices.SelectedIndex = listInvoices.Items.Count - 1;
+                RefreshState();
+            }
+            else
+            {
+                foreach (var value in values)
+                    listInvoices.Items.Remove(value);
+                RefreshState();
+            }
+        }
+
+        private void listInvoices_DoubleClick(object sender, EventArgs e)
+        {
+            if (listInvoices.SelectedIndex >= 0)
+            {
+                Process.Start(listInvoices.SelectedItem as string);
+            }
+        }
+
+        private void frmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void frmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            var items = (Array)e.Data.GetData(DataFormats.FileDrop);
+            var files = new List<string>();
+            foreach (object item in items)
+            {
+                string path = item.ToString();
+                var info = new FileInfo(path);
+
+                if (info.Attributes.HasFlag(FileAttributes.Directory))
+                    files.AddRange(Directory.GetFiles(path, "*.pdf", SearchOption.AllDirectories));
+                else if (File.Exists(path) && path.EndsWith(".pdf", StringComparison.CurrentCultureIgnoreCase))
+                    files.Add(path);
+            }
+            if (files.Any())
+            {
+                listInvoices.Items.AddRange(files.ToArray());
+                RefreshState();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            cmbMergeType.SelectedIndex = 1;
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            if (listInvoices.Items.Count == 0)
+            {
+                MessageBox.Show("请先添加至少一张发票。", "错误", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var dlg = saveFileDialog1.ShowDialog();
+            if (dlg != DialogResult.OK)
+                return;
+            var type = cmbMergeType.SelectedIndex;
+
+            var files = listInvoices.Items.Cast<string>().ToList();
+            try
+            {
+                switch (cmbMergeType.SelectedIndex)
+                {
+                    case 0:
+                        Merge1(files, saveFileDialog1.FileName);
+                        break;
+                    case 1:
+                    default:
+                        Merge2(files, saveFileDialog1.FileName);
+                        break;
+                    case 2:
+                        Merge4(files, saveFileDialog1.FileName);
+                        break;
+                }
+
+                Process.Start(saveFileDialog1.FileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "合并失败", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// 每页一张发票
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="outputFile"></param>
+        public static void Merge1(IEnumerable<string> files, string outputFile)
+        {
+            Document document = new Document();
+            try
+            {
+                using (FileStream newFileStream = new FileStream(outputFile, FileMode.Create))
+                {
+                    PdfCopy writer = new PdfCopy(document, newFileStream);
+                    if (writer == null)
+                    {
+                        return;
+                    }
+
+                    document.Open();
+
+                    foreach (string file in files)
+                    {
+                        PdfReader reader = new PdfReader(file);
+                        reader.ConsolidateNamedDestinations();
+
+                        for (int i = 1; i <= reader.NumberOfPages; i++)
+                        {
+                            PdfImportedPage page = writer.GetImportedPage(reader, i);
+                            writer.AddPage(page);
+                        }
+                        reader.Close();
+                    }
+                    writer.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                document.Close();
+            }
+        }
+
+
+        /// <summary>
+        /// 每页两张发票
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="outputFile"></param>
+        public static void Merge2(IEnumerable<string> files, string outputFile)
+        {
+            var queue = new ConcurrentQueue<string>(files);
+            var document = new Document();
+            var margin = 20;
+            try
+            {
+                var writer = PdfWriter.GetInstance(document, new FileStream(outputFile, FileMode.Create));
+                var size = PageSize.A4;
+                document.Open();
+                document.SetPageSize(size);
+                PdfContentByte cb = writer.DirectContent;
+
+                do
+                {
+                    document.NewPage();
+                    if (TryAppendPage(true) == false)
+                        break;
+                    if (TryAppendPage(false) == false)
+                        break;
+                } while (true);
+
+                bool TryAppendPage(bool top)
+                {
+                    if (queue.TryDequeue(out var p) == false)
+                        return false;
+
+                    var reader = new PdfReader(p);
+                    var page = writer.GetImportedPage(reader, 1);
+
+                    AffineTransform af = new AffineTransform();
+
+                    var scaleHeight = (size.Height - margin * 4) / 2 / page.Height;
+                    var scaleWidth = (size.Width - margin * 2) / page.Width;
+                    var scale = Math.Min(scaleHeight, scaleWidth);
+
+                    var height = ((size.Height - margin * 4) / 2 - scale * page.Height) / 2;
+                    var width = ((size.Width - margin * 2) - scale * page.Width) / 2;
+
+                    af.Translate(margin + width, margin + height + (top ? size.Height / 2 : 0));
+                    af.Scale(scale, scale);
+
+                    cb.AddTemplate(page, af);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                document.Close();
+            }
+        }
+
+
+        /// <summary>
+        /// 每页两张发票
+        /// </summary>
+        /// <param name="files"></param>
+        /// <param name="outputFile"></param>
+        public static void Merge4(IEnumerable<string> files, string outputFile)
+        {
+            var queue = new ConcurrentQueue<string>(files);
+            var document = new Document();
+            var margin = 20;
+            try
+            {
+                var writer = PdfWriter.GetInstance(document, new FileStream(outputFile, FileMode.Create));
+                var size = PageSize.A4.Rotate();
+                document.Open();
+                document.SetPageSize(size);
+                PdfContentByte cb = writer.DirectContent;
+
+                do
+                {
+                    document.NewPage();
+                    for (int i = 0; i < 4; i++)
+                    {
+                        if (TryAppendPage(i) == false)
+                            break;
+                    }
+                } while (queue.Count > 0);
+
+                bool TryAppendPage(int type)
+                {
+                    if (queue.TryDequeue(out var p) == false)
+                        return false;
+
+                    var reader = new PdfReader(p);
+                    var page = writer.GetImportedPage(reader, 1);
+
+                    AffineTransform af = new AffineTransform();
+
+                    var scaleHeight = (size.Height - margin * 4) / 2 / page.Height;
+                    var scaleWidth = (size.Width - margin * 4) / 2 / page.Width;
+                    var scale = Math.Min(scaleHeight, scaleWidth);
+
+                    var height = ((size.Height - margin * 4) / 2 - scale * page.Height) / 2;
+                    var width = ((size.Width - margin * 4) / 2 - scale * page.Width) / 2;
+
+                    switch (type)
+                    {
+                        case 2:
+                        default:
+                            af.Translate(margin + width, margin + height);
+                            break;
+                        case 3:
+                            af.Translate(margin + width + size.Width / 2, margin + height);
+                            break;
+                        case 0:
+                            af.Translate(margin + width, margin + height + size.Height / 2);
+                            break;
+                        case 1:
+                            af.Translate(margin + width + size.Width / 2, margin + height + size.Height / 2);
+                            break;
+                    }
+                    af.Scale(scale, scale);
+
+                    cb.AddTemplate(page, af);
+                    return true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                document.Close();
+            }
+        }
+
+        private void sourceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/hetaoos/PdfInvoiceMerge");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var frm = new frmAbout();
+            frm.ShowDialog();
+        }
+    }
+}
